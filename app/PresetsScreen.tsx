86fb80fca87fdd1e73804a8305cb40c06ec2af05
arikhativa <arikhativa@gmail.com>
2025-05-06 12:11:26 +0300

    feat: basic preset

--- app/PresetsScreen.tsx
+++ app/PresetsScreen.tsx
@@ -1,12 +1,22 @@
 import { useNavigation } from '@react-navigation/native';
-import { Text, View } from 'react-native';
+import { FlatList, Text, TouchableOpacity, View } from 'react-native';
 import { Button } from '~/components/ui/button';
+import { Separator } from '~/components/ui/separator';
+import { H1, H2, Large, Muted, P } from '~/components/ui/typography';
 import { Timer } from '~/db/schema';
 import useTimerListQuery from '~/hooks/useTimerListQuery';
 import { Plus } from '~/lib/icons/Plus';
+import { formatSeconds } from '~/lib/utils';
 
-function Line({ data }: { data: Timer }) {
-  return <Text>{data.id}</Text>;
+function Line({ item, navigation }: { item: Timer; navigation: any }) {
+  return (
+    <TouchableOpacity
+      className="flex-1 justify-between items-center flex-row mx-4 py-4 w-fit "
+      onPress={() => navigation.navigate('Timer', { id: item.id })}>
+      <Large>{item.id}</Large>
+      <Muted>{formatSeconds(item.duration)}</Muted>
+    </TouchableOpacity>
+  );
 }
 
 export function PresetsScreen() {
@@ -26,10 +36,13 @@ export function PresetsScreen() {
   }
 
   return (
-    <View>
-      {data.map(e => {
-        return <Line key={e.id} data={e} />;
-      })}
+    <View className="flex-1">
+      <FlatList
+        data={data}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => <Line item={item} navigation={navigation} />}
+        ItemSeparatorComponent={() => <Separator className="mx-5 w-fit" />}
+      />
     </View>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,12 @@ export function cn(...inputs: ClassValue[]) {
 export function capitalizeFirstLetter(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function formatSeconds(seconds: number): string {
+  const hrs = Math.floor(seconds / 3600);
+  const mins = Math.floor((seconds % 3600) / 60);
+  const secs = seconds % 60;
+
+  const pad = (n: number) => n.toString().padStart(2, '0');
+  return `${pad(hrs)}:${pad(mins)}:${pad(secs)}`;
+}
